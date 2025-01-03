5496d8ae4855a98c892d0ad0abd63dc008a85d6e
mohsensami <mohsensami18727@gmail.com>
2025-01-03 23:03:01 +0330

    initial new tools app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import Calculator from "./container/Calculator";
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import PageLayout from "./components/hoc/PageLayout";
+import Home from "./page/Home";
+import Portfolio from "./page/Portfolio";
+import NotFound from "./page/NotFound";
 
 function App() {
   return (
-    <>
-      <Calculator />
-    </>
+    <div className="App">
+      <Routes>
+        <Route element={<PageLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
