5e14b18ea3ffc15cd6d709153b60720e55e630e0
mohsensami <mohsensami18727@gmail.com>
2025-02-17 22:33:34 +0330

    show single page data from api and show by tailwind

--- src/page/movies/Movie.tsx
+++ src/page/movies/Movie.tsx
@@ -9,11 +9,8 @@ const Movie = () => {
   //     console.log(id);
   //   }, []);
   const getTrendingMovies = async () => {
-    const data = await fetchDataFromApi(
-      `/trending/movie/${endpoint}?page=${page}`,
-      ""
-    );
-    return data.results;
+    const data = await fetchDataFromApi(`/movie/${params.id}`, "");
+    return data;
   };
 
   const trendingMovies = useQuery({
@@ -29,32 +26,35 @@ const Movie = () => {
     <div className="bg-gray-900 text-white">
       <div className="relative">
         <img
-          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-          alt={movie.title}
+          src={`https://image.tmdb.org/t/p/original/${trendingMovies?.data?.backdrop_path}`}
+          alt={trendingMovies?.data?.title}
           className="w-full h-64 object-cover"
         />
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="relative container mx-auto p-4">
-          <h1 className="text-4xl font-bold">{movie.title}</h1>
-          <h2 className="text-xl italic">{movie.tagline}</h2>
+          <h1 className="text-4xl font-bold">{trendingMovies?.data?.title}</h1>
+          <h2 className="text-xl italic">{trendingMovies?.data?.tagline}</h2>
         </div>
       </div>
       <div className="container mx-auto p-4">
         <div className="flex flex-col md:flex-row">
           <img
-            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-            alt={movie.title}
+            src={`https://image.tmdb.org/t/p/original${trendingMovies?.data?.poster_path}`}
+            alt={trendingMovies?.data?.title}
             className="w-1/3 md:w-1/4 rounded-lg shadow-lg"
           />
           <div className="md:ml-4">
-            <p className="mt-2">Release Date: {movie.release_date}</p>
-            <p>Runtime: {movie.runtime} minutes</p>
-            <p>Genres: {movie.genres.join(", ")}</p>
+            <p className="mt-2">
+              Release Date: {trendingMovies?.data?.release_date}
+            </p>
+            <p>Runtime: {trendingMovies?.data?.runtime} minutes</p>
+            <p>Genres: {trendingMovies?.data?.genres.join(", ")}</p>
             <p>
-              Rating: {movie.vote_average} ({movie.vote_count} votes)
+              Rating: {trendingMovies?.data?.vote_average} (
+              {trendingMovies?.data?.vote_count} votes)
             </p>
             <a
-              href={movie.homepage}
+              href={trendingMovies?.data?.homepage}
               className="mt-4 inline-block bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
               Visit Official Site
@@ -62,10 +62,10 @@ const Movie = () => {
           </div>
         </div>
         <h3 className="mt-4 text-2xl">Overview</h3>
-        <p className="mt-2">{movie.overview}</p>
+        <p className="mt-2">{trendingMovies?.data?.overview}</p>
         <h3 className="mt-4 text-xl">Production Companies</h3>
         <div className="flex flex-wrap mt-2">
-          {movie.production_companies.map((company, index) => (
+          {trendingMovies?.data?.production_companies.map((company, index) => (
             <div key={index} className="flex items-center mr-4">
               <img
                 src={`https://image.tmdb.org/t/p/w500${company.logo_path}`}
