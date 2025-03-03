45e3647cd037804ecf49c1fdf37427ff77482882
mohsensami <mohsensami18727@gmail.com>
2025-03-03 22:50:23 +0330

    working on Home page

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      <div className="bg-white flex justify-between">Movie</div>
+      <div className="bg-white flex justify-between">Dictionary</div>
+      <div className="bg-white flex justify-between"></div>
+      <div className="bg-white flex justify-between"></div>
+    </div>
+  );
 };
 
 export default Home;
