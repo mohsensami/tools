a878a556e11e9a0c614586a16a3e5737eb4824b1
mohsensami <mohsensami18727@gmail.com>
2025-05-05 22:22:47 +0330

    select box to generate lorem

--- src/page/Lorem.tsx
+++ src/page/Lorem.tsx
@@ -1,120 +1,73 @@
+import { useState } from "react";
+
 const Lorem = () => {
+  const [numParagraphs, setNumParagraphs] = useState(1);
+  const [generatedText, setGeneratedText] = useState("");
+
+  const loremIpsum = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+    "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.",
+    "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.",
+    "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.",
+    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.",
+    "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi.",
+  ];
+
+  const generateLorem = () => {
+    const paragraphs = [];
+    for (let i = 0; i < numParagraphs; i++) {
+      paragraphs.push(loremIpsum[i % loremIpsum.length]);
+    }
+    setGeneratedText(paragraphs.join("\n\n"));
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 py-8">
-      <div className="max-w-7xl mx-auto px-4">
+      <div className="max-w-3xl mx-auto px-4">
         <div className="text-center mb-8">
-          <h1 className="text-3xl font-bold text-gray-900">
-            Power Supply Calculator
+          <h1 className="text-3xl font-bold text-gray-900 mb-4">
+            Lorem Ipsum Generator
           </h1>
-          <p className="mt-2 text-gray-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-            dolore tempora dolorum. Quibusdam libero et quisquam fugiat id ullam
-            qui veniam modi dignissimos tenetur, pariatur, corporis doloribus
-            optio ut maxime. Enim doloremque culpa a aliquid explicabo similique
-            qui non ducimus possimus libero, ex odio reprehenderit fuga vel ab
-            dolor voluptates at veniam rem dicta. Distinctio sed nulla minus!
-            Ipsum, voluptatibus. Quidem iure, porro voluptas cupiditate
-            perferendis odit ipsam temporibus pariatur sed, voluptatum
-            aspernatur ratione architecto a rem ipsum odio culpa est. Harum
-            aliquam ad quod repellendus cum maiores quisquam fuga! Sunt quaerat
-            repellat reiciendis laudantium iure accusamus excepturi incidunt
-            dicta, alias autem placeat repellendus quo illo expedita ab
-            aspernatur reprehenderit, fuga dignissimos ducimus atque, quis
-            deleniti. Repudiandae quo soluta quasi. Possimus placeat mollitia,
-            provident quia fugiat recusandae! Quo ex doloremque velit obcaecati
-            voluptas? Dolorum in sapiente, porro reprehenderit aspernatur
-            perspiciatis cupiditate placeat dolorem accusamus asperiores quasi
-            illo dolores est eveniet. Modi officiis beatae ut repudiandae
-            facilis ipsam esse perspiciatis inventore? Ducimus cum reiciendis,
-            qui illum fugiat sed itaque rem optio, possimus repudiandae autem
-            consectetur maiores deserunt. Ullam dolorum ea ipsa. Quasi,
-            officiis? Eum nesciunt fugiat ea architecto sint officiis, tempora
-            velit aspernatur dolorum aliquam tempore dolorem quidem veritatis
-            adipisci in omnis repellat unde, autem pariatur voluptas eveniet
-            fuga non. Doloremque! Voluptates aliquam voluptate quae provident id
-            nesciunt, dolores excepturi quasi, earum dolorem sequi minus sit ad
-            reprehenderit totam in. Quod, tempora ex! Distinctio rerum
-            praesentium sed iusto voluptate sit perferendis. Praesentium officia
-            illum, quod aspernatur similique porro. Repudiandae molestiae eos
-            dolorum, voluptatum possimus sed laborum illum magni atque at
-            voluptate distinctio eligendi unde libero amet quam consequuntur
-            aspernatur officia numquam! Eveniet vero facilis culpa suscipit
-            iure, facere et laborum voluptatibus quas. Voluptates, cumque nemo
-            fugit voluptatem corrupti tenetur quidem vitae exercitationem
-            laborum sed accusamus quibusdam, quisquam quae, esse et labore.
-            Consectetur officiis temporibus dolores. Id doloribus hic aliquid
-            dicta molestiae consequatur incidunt error. Voluptate, odit mollitia
-            illum ducimus provident aliquid. Placeat laboriosam eligendi
-            deleniti maiores illo possimus labore nesciunt eaque! Pariatur,
-            esse. Autem, eum numquam, animi nemo voluptatibus vitae iure cumque
-            quos ut necessitatibus voluptatum officiis ipsum architecto at
-            distinctio nisi? Consequuntur necessitatibus expedita et earum
-            reiciendis sapiente facilis eius? Libero voluptatum consectetur qui
-            quisquam? Mollitia consequatur recusandae eos, consectetur, ipsa
-            voluptatum libero ullam culpa minus quasi odit vel laboriosam autem
-            quis corrupti quibusdam cupiditate accusamus earum alias
-            praesentium. Molestiae. Nesciunt veritatis repellendus commodi
-            distinctio quo consequuntur quos unde autem ut nobis deserunt
-            numquam omnis expedita similique mollitia suscipit fuga magni ab
-            itaque blanditiis, facilis reiciendis. Adipisci officiis voluptates
-            dolore. Dolorum, iure sit ducimus laborum necessitatibus molestiae
-            itaque deleniti deserunt natus aliquid? Itaque deleniti eaque odit!
-            Minima doloribus veniam natus quia dolore fugiat adipisci magni
-            deserunt reiciendis neque? Eum, libero. Officiis architecto esse
-            sint impedit veniam vero minima, repudiandae necessitatibus corporis
-            nisi explicabo quo est, facilis earum consequuntur expedita
-            molestiae adipisci a ea, tempora aut veritatis ipsa repellat
-            voluptates. Magni. Repellat natus quisquam, ratione a mollitia nulla
-            doloremque consectetur ullam enim dolor alias est perspiciatis ex
-            voluptatem harum atque accusantium nobis eaque recusandae fugit eos.
-            Impedit ducimus delectus inventore provident! Nam itaque excepturi
-            aspernatur velit voluptatum beatae minus, quidem vel. Debitis,
-            saepe. Quia, velit vitae! Doloremque soluta cumque explicabo velit
-            ad! Sint, ipsum molestias? Necessitatibus, in. Voluptatibus alias et
-            illum. Error quibusdam accusantium consectetur! Voluptatem, minima
-            aliquam animi aliquid fugit eligendi ipsa, at, saepe beatae iusto
-            doloribus soluta repudiandae excepturi maiores voluptatibus tempora.
-            Ratione dignissimos facere aspernatur exercitationem quae atque!
-            Dolorum in assumenda veniam! Officiis aliquid, tenetur odio
-            veritatis quam laboriosam atque amet eligendi quasi aspernatur eum
-            unde possimus quas et, distinctio mollitia dolore velit saepe
-            maiores commodi quisquam corrupti. Possimus et minima quia
-            perferendis, fuga porro, ea veniam sed repudiandae, vitae animi
-            asperiores ullam perspiciatis cupiditate amet repellendus hic eius
-            neque consectetur tempora corporis odio earum harum? Quisquam,
-            velit. Beatae esse nam minus amet magnam, harum libero neque animi
-            nobis iusto laudantium debitis illum facilis nisi, rerum illo. Quia
-            voluptatibus tenetur facilis voluptate quisquam ratione asperiores
-            officiis totam neque. Temporibus dolores ipsam accusantium eaque
-            iure. Atque repellendus ab officiis maxime nesciunt? Sequi atque
-            aperiam quaerat? Cupiditate dolorum explicabo nam, fugiat recusandae
-            error earum officia ipsam. Mollitia dolorum optio deleniti. Optio,
-            exercitationem animi temporibus debitis delectus ex est atque, ab
-            omnis quos nemo. Porro quos dolore repellat voluptas maiores magni
-            hic recusandae debitis quia, consequatur obcaecati iste corporis.
-            Saepe, voluptas. Enim laborum cum mollitia veritatis optio harum,
-            illo delectus possimus voluptatum magni sint illum cumque? Assumenda
-            doloremque vitae quisquam, mollitia, ratione, saepe adipisci est
-            tenetur voluptate vero molestias voluptatem reprehenderit. Excepturi
-            numquam maiores soluta iusto veniam beatae, minima quam at esse
-            explicabo. Perferendis, fugit fugiat. Sit suscipit eaque
-            necessitatibus voluptatibus nostrum natus repudiandae provident
-            dolore, quasi ducimus delectus, vel earum! Iusto magnam mollitia
-            consequatur quis eligendi unde est fugit sapiente, quibusdam magni
-            quo. Enim doloribus vitae necessitatibus, nesciunt repellat minima
-            labore, quisquam laborum libero neque ad hic tempora debitis ipsa.
-            Pariatur culpa quam voluptate quod debitis aliquid error esse
-            voluptatibus labore molestias at nesciunt deleniti quas, explicabo
-            adipisci, ipsam ex eius illo, corrupti eligendi! Dolorum doloribus
-            amet unde porro tempora. Reprehenderit ipsam reiciendis accusantium
-            culpa quasi nobis magni sit molestias perferendis placeat, at quis,
-            modi veritatis aliquam cumque perspiciatis fugiat, ab sint eligendi
-            tempora maiores? Excepturi consequatur soluta sunt error. Ab
-            repellendus reprehenderit quos? Aspernatur, vel quis iure quo esse
-            expedita vero fuga! Alias et tempora quos sequi rerum quam? In
-            eligendi culpa odio maxime quibusdam, deserunt sint hic officia.
-          </p>
+          <div className="flex flex-col items-center gap-4">
+            <div className="w-full max-w-xs">
+              <label
+                htmlFor="paragraphs"
+                className="block text-sm font-medium text-gray-700 mb-2"
+              >
+                Number of Paragraphs
+              </label>
+              <select
+                id="paragraphs"
+                value={numParagraphs}
+                onChange={(e) => setNumParagraphs(Number(e.target.value))}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              >
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => (
+                  <option key={num} value={num}>
+                    {num}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <button
+              onClick={generateLorem}
+              className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Generate Lorem Ipsum
+            </button>
+          </div>
         </div>
+        {generatedText && (
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <div className="whitespace-pre-line text-gray-700">
+              {generatedText}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
