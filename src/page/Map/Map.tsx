8c832c90bf841f680416c7a4d294a0f489586ffc
mohsensami <mohsensami18727@gmail.com>
2025-07-17 21:57:23 +0330

    leaflet openstreetmap added

--- src/page/Map/Map.tsx
+++ src/page/Map/Map.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { LatLngExpression } from "leaflet";
+
+const center: LatLngExpression = [32.4279, 53.688];
 
 const Map = () => {
-  return <div>Map</div>;
+  return (
+    <MapContainer
+      center={center}
+      zoom={6}
+      style={{ height: "800px", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={center}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 };
 
 export default Map;
